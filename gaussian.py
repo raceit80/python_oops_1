4c25bd5f5bc1d0072e6122012e4e2a319f49bbb6
Ravi Subramanian <ravi.subram@gmail.com>
2020-08-28 17:39:21 -0500

    included methods for calculating mean, standard deviation and reading data

--- gaussian.py
+++ gaussian.py
@@ -17,4 +17,57 @@ class Gaussian():
         self.stdev = sigma
         self.data = []  #this is an empty list for now to hold the data
             
-    
+    def read_data_file(self, filepath, sample=True):
+        """Function to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute. 
+        After reading in the file, the mean and standard deviation are calculated
+                
+        Args:
+            file_name (string): name of a file to read from
+        
+        Returns:
+            None
+        
+        """
+        with open(filepath) as file:
+            data_list = []
+            line = file.readline()
+            while line:
+                data_list.append(int(line))
+                line = file.readline()
+            self.data = data_list
+            mean = self.calculate_mean()
+            stdev = self.calculate_stdev(sample)
+            
+            
+        def calculate_mean(self):
+            mean = 1.00 * sum(self.data) / len(self.data)
+            self.mean = mean
+            return mean
+
+
+
+        def calculate_stdev(self, sample=True):
+            if sample:
+                n = len(self.data) - 1 #length or sample
+            else:
+                n = len(self.data) #length for population
+           
+            mean = self.mean
+            sigma = 0
+
+            for data in self.data:
+                sigma+=(data-mean)**2
+            sigma = math.sqrt(sigma/n)
+
+            self.stdev = sigma
+            return sigma
+            
+
+
+
+
+
+
+
+
